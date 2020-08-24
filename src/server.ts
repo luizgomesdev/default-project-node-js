bfd7d8803e6cdca99d67970ebae11d0f2ca33598
Luiz Gomes <luizgomesdesenvolvimento@gmail.com>
2020-08-24 16:36:46 -0300

    Finalizando

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+app.use(routes);
+
 app.get('/', (request, response) => {
     return response.json({ message: 'Hello World' });
 });
